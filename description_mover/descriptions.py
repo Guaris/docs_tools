e6100d901b2c80e10ff00cf9bc2ca8b215b0239f
Angel <angel.guarisma@konghq.com>
2023-03-30 09:13:08 -0400

    nested fields

--- description_mover/descriptions.py
+++ description_mover/descriptions.py
@@ -1,30 +1,45 @@
 import json
+import re
 
 
-def insert_description(schema, json_data):
+def insert_description(schema, json_data, parent_keys=None):
+    if parent_keys is None:
+        parent_keys = []
+
     for field in json_data["fields"]:
         for key, value in field.items():
+            current_keys = parent_keys + [key]
+
             if "fields" in value:
-                schema = insert_description(schema, value)
+                schema = insert_description(schema, value, current_keys)
+
             if "description" in value:
                 search_pattern = f"{key} = {{"
                 formatted_description = value["description"].replace("\n", "\\n").replace('"', '\\"')
                 replace_pattern = f'{key} = {{ description = "{formatted_description}", '
-                schema = schema.replace(search_pattern, replace_pattern)
-            else:
-                print(f"Field '{key}' does not have a description.")
 
-            if "elements" in value:
-                if "fields" in value["elements"]:
-                    schema = insert_description(schema, value["elements"])
+                if search_pattern in schema:
+                    schema = schema.replace(search_pattern, replace_pattern)
+                else:
+                    nested_key_pattern = " = {"
+                    nested_search_pattern = nested_key_pattern.join(current_keys) + " = {"
+                    nested_replace_pattern = nested_key_pattern.join(current_keys) + " = { description = \"" + formatted_description + "\", "
+
+                    if re.search(re.escape(nested_search_pattern), schema):
+                        schema = re.sub(re.escape(nested_search_pattern), nested_replace_pattern, schema)
+                    else:
+                        print(f"Couldn't insert description for nested field '{key}' in the schema:\n{formatted_description}\n")
+
+            if "elements" in value and "fields" in value["elements"]:
+                schema = insert_description(schema, value["elements"], current_keys)
 
     return schema
 
 
-with open("datadog.lua", "r") as f:
+with open("acme.lua", "r") as f:
     lua_schema = f.read()
 
-with open("datadog.json", "r") as f:
+with open("acme.json", "r") as f:
     json_data = json.load(f)
 
 updated_schema = insert_description(lua_schema, json_data)
--- description_mover/updated_schema.lua
+++ description_mover/updated_schema.lua
@@ -1,110 +1,190 @@
 local typedefs = require "kong.db.schema.typedefs"
+local reserved_words = require "kong.plugins.acme.reserved_words"
 
-local STAT_NAMES = {
-  "kong_latency",
-  "latency",
-  "request_count",
-  "request_size",
-  "response_size",
-  "upstream_latency",
+local CERT_TYPES = { "rsa", "ecc" }
+
+local RSA_KEY_SIZES = { 2048, 3072, 4096 }
+
+local STORAGE_TYPES = { "kong", "shm", "redis", "consul", "vault" }
+
+local function validate_namespace(namespace)
+  if namespace ~= "" then
+    for _, v in pairs(reserved_words) do
+      if namespace:sub(1, #v) == v then
+        return nil, "namespace can't be prefixed with reserved word: " .. v
+      end
+    end
+  end
+
+  return true
+end
+
+local SHM_STORAGE_SCHEMA = {
+  { shm_name = {
+    type = "string",
+    default = "kong",
+    custom_validator = function(d) return ngx.shared[d] end,
+  }, },
 }
 
-local STAT_TYPES = {
-  "counter",
-  "gauge",
-  "histogram",
-  "meter",
-  "set",
-  "timer",
-  "distribution",
+-- must be a table per schema definition
+local KONG_STORAGE_SCHEMA = {
 }
 
-local CONSUMER_IDENTIFIERS = {
-  "consumer_id",
-  "custom_id",
-  "username",
+local REDIS_STORAGE_SCHEMA = {
+  { host = typedefs.host, },
+  { port = typedefs.port, },
+  { database = { type = "number" }},
+  { auth = { type = "string", referenceable = true, }},
+  { ssl = { type = "boolean", required = true, default = false } },
+  { ssl_verify = { type = "boolean", required = true, default = false } },
+  { ssl_server_name = typedefs.sni { required = false } },
+  { namespace = { type = "string", required = true, default = "", len_min = 0,
+                  custom_validator = validate_namespace } },
 }
 
-local DEFAULT_METRICS = {
-  {
-    name        = "request_count",
-    stat_type   = "counter",
-    sample_rate = 1,
-    tags        = {"app:kong" },
-    consumer_identifier = "custom_id"
-  },
-  {
-    name      = "latency",
-    stat_type = "timer",
-    tags      = {"app:kong"},
-    consumer_identifier = "custom_id"
-  },
-  {
-    name      = "request_size",
-    stat_type = "timer",
-    tags      = {"app:kong"},
-    consumer_identifier = "custom_id"
-  },
-  {
-    name      = "response_size",
-    stat_type = "timer",
-    tags      = {"app:kong"},
-    consumer_identifier = "custom_id"
-  },
-  {
-    name      = "upstream_latency",
-    stat_type = "timer",
-    tags      = {"app:kong"},
-    consumer_identifier = "custom_id"
-  },
-  {
-    name      = "kong_latency",
-    stat_type = "timer",
-    tags      = {"app:kong"},
-    consumer_identifier = "custom_id"
-  },
+local CONSUL_STORAGE_SCHEMA = {
+  { https = { type = "boolean", default = false, }, },
+  { host = typedefs.host, },
+  { port = typedefs.port, },
+  { kv_path = { type = "string", }, },
+  { timeout = { type = "number", }, },
+  { token = { type = "string", referenceable = true, }, },
 }
 
+local VAULT_STORAGE_SCHEMA = {
+  { https = { type = "boolean", default = false, }, },
+  { host = typedefs.host, },
+  { port = typedefs.port, },
+  { kv_path = { type = "string", }, },
+  { timeout = { type = "number", }, },
+  { token = { type = "string", referenceable = true, }, },
+  { tls_verify = { type = "boolean", default = true, }, },
+  { tls_server_name = { type = "string" }, },
+  { auth_method = { type = "string", default = "token", one_of = { "token", "kubernetes" } } },
+  { auth_path =  { type = "string" }, },
+  { auth_role =  { type = "string" }, },
+  { jwt_path =  { type = "string" }, },
+}
 
-return {
-  name = "datadog",
+local ACCOUNT_KEY_SCHEMA = {
+  { key_id = { type = "string", required = true }},
+  { key_set = { type = "string" }}
+}
+
+local schema = {
+  name = "acme",
   fields = {
-    { protocols = typedefs.protocols },
+    -- global plugin only
+    { consumer = typedefs.no_consumer },
+    { service = typedefs.no_service },
+    { route = typedefs.no_route },
+    { protocols = typedefs.protocols_http },
     { config = {
-        type = "record",
-        fields = {
-          { host = typedefs.host({ default = "localhost" }), },
-          { port = typedefs.port({ default = 8125 }), },
-          { prefix = { description = "String to be attached as a prefix to a metric's name.",  type = "string", default = "kong" }, },
-          { service_name_tag = { description = "String to be attached as the name of the service.",  type = "string", default = "name"}, },
-          { status_tag = { description = "String to be attached as the tag of the HTTP status.",  type = "string", default = "status" }, },
-          { consumer_tag = { description = "String to be attached as tag of the consumer.",  type = "string", default = "consumer" }, },
-          { retry_count = { description = "Number of times to retry when sending data to the upstream server.",  type = "integer", required = true, default = 10 }, },
-          { queue_size = { description = "Maximum number of log entries to be sent on each message to the upstream server.",  type = "integer", required = true, default = 1 }, },
-          { flush_timeout = { description = "Optional time in seconds. If `queue_size` > 1, this is the max idle time before sending a log with less than `queue_size` records.",  type = "number", required = true, default = 2 }, },
-          { metrics = { description = "List of metrics to be logged. Available values are described at [Metrics](#metrics).\nBy default, the plugin logs all available metrics. If you specify an array of metrics,\nonly the listed metrics are logged.", 
-              type     = "array",
-              required = true,
-              default  = DEFAULT_METRICS,
-              elements = {
-                type = "record",
-                fields = {
-                  { name = { type = "string", required = true, one_of = STAT_NAMES }, },
-                  { stat_type = { description = "test",  type = "string", required = true, one_of = STAT_TYPES }, },
-                  { tags = { type = "array", elements = { type = "string", match = "^.*[^:]$" }, }, },
-                  { sample_rate = { type = "number", between = { 0, 1 }, }, },
-                  { consumer_identifier = { type = "string", one_of = CONSUMER_IDENTIFIERS }, },
-                },
-                entity_checks = {
-                  { conditional = {
-                    if_field = "stat_type",
-                    if_match = { one_of = { "counter", "gauge" }, },
-                    then_field = "sample_rate",
-                    then_match = { required = true },
-          }, }, }, }, }, },
+      type = "record",
+      fields = {
+        { account_email = { description = "The account identifier. Can be reused in a different plugin instance.", 
+          type = "string",
+          -- very loose validation for basic sanity test
+          match = "%w*%p*@+%w*%.?%w*",
+          required = true,
+          encrypted = true, -- Kong Enterprise-exclusive feature, does nothing in Kong CE
+          referenceable = true,
+        }, },
+        { account_key = {
+          type = "record",
+          required = false,
+          fields = ACCOUNT_KEY_SCHEMA,
+        }, },
+        { api_uri = typedefs.url({ default = "https://acme-v02.api.letsencrypt.org/directory" }),
         },
+        { tos_accepted = { description = "If you are using Let's Encrypt, you must set this to `true` to agree the [Terms of Service](https://letsencrypt.org/repository/).", 
+          type = "boolean",
+          default = false,
+        }, },
+        { eab_kid = { description = "External account binding (EAB) key id. You usually don't need to set this unless it is explicitly required by the CA.", 
+          type = "string",
+          encrypted = true, -- Kong Enterprise-exclusive feature, does nothing in Kong CE
+          referenceable = true,
+        }, },
+        { eab_hmac_key = { description = "External account binding (EAB) base64-encoded URL string of the HMAC key. You usually don't need to set this unless it is explicitly required by the CA.", 
+          type = "string",
+          encrypted = true, -- Kong Enterprise-exclusive feature, does nothing in Kong CE
+          referenceable = true,
+        }, },
+        -- Kong doesn't support multiple certificate chains yet
+        { cert_type = { description = "The certificate type to create. The possible values are `'rsa'` for RSA certificate or `'ecc'` for EC certificate.", 
+          type = "string",
+          default = 'rsa',
+          one_of = CERT_TYPES,
+        }, },
+        { rsa_key_size = { description = "RSA private key size for the certificate. The possible values are 2048, 3072, or 4096.", 
+          type = "number",
+          default = 4096,
+          one_of = RSA_KEY_SIZES,
+        }, },
+        { renew_threshold_days = { description = " Days remaining to renew the certificate before it expires.", 
+          type = "number",
+          default = 14,
+        }, },
+        { domains = typedefs.hosts },
+        { allow_any_domain = { description = "If set to `true`, the plugin allows all domains and ignores any values in the `domains` list.", 
+          type = "boolean",
+          default = false,
+        }, },
+        { fail_backoff_minutes = { description = "Minutes to wait for each domain that fails to create a certificate. This applies to both a\nnew certificate and a renewal certificate.", 
+          type = "number",
+          default = 5,
+        }, },
+        { storage = { description = "The backend storage type to use. The possible values are `'kong'`, `'shm'`, `'redis'`, `'consul'`, or `'vault'`. In DB-less mode, `'kong'` storage is unavailable. Note that `'shm'` storage does not persist during Kong restarts and does not work for Kong running on different machines, so consider using one of `'kong'`, `'redis'`, `'consul'`, or `'vault'` in production. Please refer to the Hybrid Mode sections below as well.", 
+          type = "string",
+          default = "shm",
+          one_of = STORAGE_TYPES,
+        }, },
+        { storage_config = {
+          type = "record",
+          fields = {
+            { shm = { type = "record", fields = SHM_STORAGE_SCHEMA, } },
+            { kong = { type = "record", fields = KONG_STORAGE_SCHEMA, } },
+            { redis = { type = "record", fields = REDIS_STORAGE_SCHEMA, } },
+            { consul = { type = "record", fields = CONSUL_STORAGE_SCHEMA, } },
+            { vault = { type = "record", fields = VAULT_STORAGE_SCHEMA, } },
+          },
+        }, },
+        { preferred_chain = {
+          type = "string",
+        }, },
+        { enable_ipv4_common_name = {
+          type = "boolean",
+          default = true,
+        }, },
       },
-    },
+    }, },
+  },
+  entity_checks = {
+    { conditional = {
+      if_field = "config.api_uri", if_match = { one_of = {
+        "https://acme-v02.api.letsencrypt.org",
+        "https://acme-staging-v02.api.letsencrypt.org",
+      } },
+      then_field = "config.tos_accepted", then_match = { eq = true },
+      then_err = "terms of service must be accepted, see https://letsencrypt.org/repository/",
+    } },
+    { custom_entity_check = {
+      field_sources = { "config.storage", },
+      fn = function(entity)
+        local field = entity.config.storage
+        if _G.kong and kong.configuration.database == "off" and
+            kong.configuration.role ~= "data_plane" and field == "kong" then
+          return nil, "\"kong\" storage can't be used with dbless mode"
+        end
+        if _G.kong and kong.configuration.role == "control_plane" and field == "shm" then
+          return nil, "\"shm\" storage can't be used in Hybrid mode"
+        end
+        return true
+      end
+    } },
   },
 }
 
+return schema
